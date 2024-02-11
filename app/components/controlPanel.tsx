6f46414041779810d24ce138c7e1d80d4b6157f5
Finns841594 <fengy142857@gmail.com>
2024-02-11 22:21:38 +0100

    set value display for dates

--- app/components/controlPanel.tsx
+++ app/components/controlPanel.tsx
@@ -1,21 +1,32 @@
 import { Slider } from '@nextui-org/react';
+import { useState } from 'react';
 
 const daysMark = [
   {
-    value: 20,
-    label: '20%',
+    value: 79, // 20 March
+    label: 'spring equinox',
   },
   {
-    value: 170,
+    value: 171, // 20 June
     label: 'summer soltstice',
   },
   {
-    value: 330,
+    value: 242, // 20 June
+    label: 'autumnal soltstice',
+  },
+  {
+    value: 355, // 21 Dec
     label: 'winter soltstice',
   },
 ];
 
 const ControlPanel = () => {
+  const [dateInNumber, setDateInNumber] = useState<number>(1);
+  const handleChange = (value: number | number[]) => {
+    const newValue = Array.isArray(value) ? value[0] : value;
+    setDateInNumber(newValue);
+    console.log(`Selected value: ${newValue}`);
+  };
   return (
     <div className="md:w-3/4 -mt-20 mx-auto flex md:flex-row gap-8 md:justify-between">
       <Slider
@@ -24,9 +35,17 @@ const ControlPanel = () => {
         marks={daysMark}
         step={1}
         maxValue={365}
-        minValue={0}
+        minValue={1}
+        value={dateInNumber}
+        onChange={handleChange}
         defaultValue={0}
         className="max-w-md"
+        showTooltip
+        tooltipProps={{
+          content:
+            daysMark.find(item => item.value === dateInNumber)?.label ||
+            dateInNumber,
+        }}
       />
       <Slider
         label="Latitude"
