dd8c334c44b6bbe86ff5f8aca8cffb21fc7bb40e
Finns841594 <fengy142857@gmail.com>
2024-03-07 14:30:08 +0100

    changed link style

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,10 +6,12 @@ const Header = () => {
       <div className="flex justify-between px-2 md:px-4">
         <p className="text-xl font-bold">Why Midnight Sun</p>
         <Link
+          isExternal
           href="https://feng-y.notion.site/Why-Midnight-Sun-Dev-Notes-f6bfb29cc9af46a4b9dac9f4f5441e87?pvs=4"
           color="foreground"
+          showAnchorIcon
         >
-          About
+          Development Note
         </Link>
       </div>
       <Divider />
